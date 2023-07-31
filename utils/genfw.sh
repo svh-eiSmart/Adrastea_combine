8c61f36ce47c580654d9afd3de14d635477b3171
Shashank Hegde <105643484+svh-eiSmart@users.noreply.github.com>
2023-07-31 10:00:25 +0200

    Delete utils directory

--- utils/genfw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-arm-none-eabi-objcopy -O binary "${1%bin}elf" "$1"
-
-if [[ -n "${ComSpec}" ]]; then
-	CMD="${ComSpec} /c"
-fi
-
-${CMD} "$(dirname "$0")/genfw.py" "$1"
--- utils/genver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ -n "${ComSpec}" ]]; then
-	CMD="${ComSpec} /c"
-fi
-
-${CMD} "$(dirname "$0")/genver.py" $1
